b888d250fc50d673373babd77b04b8dd01539b14
Keyla-Auikyani <anamuzquiz4@gmail.com>
2023-10-22 22:14:09 -0500

    addedEnemies_and_EnemyMovement

--- Assets/spawnEnemies.cs
+++ Assets/spawnEnemies.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class spawnEnemies : MonoBehaviour
 {
     public GameObject enemy;
-    private Vector3 enemyPosition;
-    // Start is called before the first frame update
+    public Vector3 spawnPosition;
+    public float spawnRadius;
+    public float spawnInterval = 3f;
+
+    void SpawnEnemy()
+    {
+        float randomY = Random.Range(-spawnRadius, spawnRadius);
+        Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y + randomY, transform.position.z);
+        Instantiate(enemy, spawnPosition, Quaternion.identity);
+    }
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        enemyPosition
+        InvokeRepeating("SpawnEnemy", 0f, spawnInterval);
     }
 }
+
